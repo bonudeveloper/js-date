28b690e6559751a8f169d8aeee598be88bad2d31
Kumush Xusanova <127647054+bonudeveloper@users.noreply.github.com>
2023-10-25 22:39:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//1-vazifa
+let ism = prompt("hurmatli foydalanuvchi ismingizni kiriting");
+
+let ismKotta = ism.toUpperCase();
+let bYear = prompt("tug'ilgan yilingizni kiriting");
+let bMonths = prompt("tug'ilgan oyingizni kiriting masalan:09");
+let bday = prompt("tug'ilgan kuningizni kiriting masalan:09");
+let hozir = Date.now();
+
+let kiritilgan = new Date(bYear, bMonths - 1, bday).getTime();
+let yaxlidYear = parseInt((hozir-kiritilgan)/1000/60/60/24/365.25);
+let yaxlidMonths = parseInt(((hozir-kiritilgan)/1000/60/60/24)/12);
+let yaxlidbday = parseInt(((hozir-kiritilgan)/1000/60/60/24));
+let javob = alert('Hurmatli' + ' ' + ismKotta + ' ' + 'SIZ' + ' ' + yaxlidYear + ' ' + 'yil' + ' ' + yaxlidMonths + ' ' + 'oy' + ' ' + yaxlidbday + ' ' + 'kun' + ' ' + 'yashabsiz');
+
+// 2-vazifa
+//consolga mevalar
+let arr = ["banan", "olma", "nok"];
+console.log(arr);
+
+// meva oxiriga qo'shish
+arr.push("olcha");
+console.log(arr);
+
+// meva boshidan bitta o'chsin ,consolga chiqsin
+arr.shift();
+console.log(arr);
