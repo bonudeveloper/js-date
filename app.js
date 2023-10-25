c34f7a5ea59f823ccee46e22464509e4ce8cd52c
Kumush Xusanova <127647054+bonudeveloper@users.noreply.github.com>
2023-10-25 22:48:45 +0500

    second commit

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ let ismKotta = ism.toUpperCase();
 let bYear = prompt("tug'ilgan yilingizni kiriting");
 let bMonths = prompt("tug'ilgan oyingizni kiriting masalan:09");
 let bday = prompt("tug'ilgan kuningizni kiriting masalan:09");
+let time = prompt("hozirgi vaqtni(soat) kiriting");
 let hozir = Date.now();
 
 let kiritilgan = new Date(bYear, bMonths - 1, bday).getTime();
 let yaxlidYear = parseInt((hozir-kiritilgan)/1000/60/60/24/365.25);
 let yaxlidMonths = parseInt(((hozir-kiritilgan)/1000/60/60/24)/12);
 let yaxlidbday = parseInt(((hozir-kiritilgan)/1000/60/60/24));
-let javob = alert('Hurmatli' + ' ' + ismKotta + ' ' + 'SIZ' + ' ' + yaxlidYear + ' ' + 'yil' + ' ' + yaxlidMonths + ' ' + 'oy' + ' ' + yaxlidbday + ' ' + 'kun' + ' ' + 'yashabsiz');
+let yaxlidTime = parseInt((hozir - kiritilgan)/1000/60/60);
+let javob = alert('Hurmatli' + ' ' + ismKotta + ' ' + 'SIZ' + ' ' + yaxlidYear + ' ' + 'yil' + ' ' + yaxlidMonths + ' ' + 'oy' + ' ' + yaxlidbday + ' ' + 'kun' + ' ' + yaxlidTime + ' ' + 'soat' +  ' ' +'yashabsiz');
 
 // 2-vazifa
 //consolga mevalar
